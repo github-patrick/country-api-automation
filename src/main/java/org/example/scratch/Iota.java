1bea3d729d8d8d9500ce1384edae77d90658298f
patrick <developer.session@gmail.com>
2020-04-04 15:13:06 +0100

    committing the base64 encoder code

--- src/main/java/org/example/scratch/Iota.java
+++ src/main/java/org/example/scratch/Iota.java
@@ -1,6 +1,15 @@
 package org.example.scratch;
 
 
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class Iota {
 
     // encapsulation of the internal state of the class
@@ -24,21 +33,30 @@ public class Iota {
 
     static class Hello {
         private int [] ages = new int[3];
-        public void calculateTotalAges() {
+        public void calculateTotalAges() throws UnsupportedEncodingException {
             ages[0] = 45;
             ages[1] = 34;
-            ages[2] = 19;
+            ages[2] = 33;
             int totalAge = 0;
             for (int a : ages) {
                 totalAge+=a;
             }
+
+
+           Integer integer = Arrays.stream(ages).filter((s) -> s > 20).findFirst().orElse(45);
+
+            String encoded = Base64.getEncoder().encodeToString("tester:password".getBytes());
+            String urlEncoded = Base64.getUrlEncoder().encodeToString("https://google.com?q=this&that".getBytes("utf-8"));
+            System.out.println("Base 64: " + encoded);
+            System.out.println("Base 64 url: " + urlEncoded);
+
+
+
             System.out.println("The average age: " + totalAge/ages.length);
         }
-
-
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Iota iota = new Iota();
         Deep deep = iota.new Deep();
